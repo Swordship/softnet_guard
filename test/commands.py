27dea49f3cb279f98a98068a90e9bd1746db5f50
Monish <monish635112@gmail.com>
2026-03-03 08:42:11 +0530

    before it was like lines of the result but now we need in correct form of ip and mac address difference so we split it , because we will use this for other place where we need correct form .

--- test/commands.py
+++ test/commands.py
@@ -1,6 +1,12 @@
 import subprocess
 result = subprocess.run(['arp', '-a'] , capture_output= True, text=True)
 lines = result.stdout.splitlines()
+# for line in lines:
+#     if 'dynamic' in line:
+#         print(line)
+output = []
 for line in lines:
     if 'dynamic' in line:
-        print(line)
+        ip, mac = line.split()[:2]
+        output.append({"ip": ip, "mac": mac})
+print(output)
