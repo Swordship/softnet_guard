c659dbe3901a136dac3c874772df2477c6acf0ee
Monish <monish635112@gmail.com>
2026-03-03 10:20:37 +0530

    test file corrected a

--- test/commands.py
+++ test/commands.py
@@ -1,12 +1,25 @@
-import subprocess
-result = subprocess.run(['arp', '-a'] , capture_output= True, text=True)
-lines = result.stdout.splitlines()
+# import subprocess
+# result = subprocess.run(['arp', '-a'] , capture_output= True, text=True)
+# lines = result.stdout.splitlines()
+# # for line in lines:
+# #     if 'dynamic' in line:
+# #         print(line)
+# output = []
 # for line in lines:
 #     if 'dynamic' in line:
-#         print(line)
+#         ip, mac = line.split()[:2]
+#         output.append({"ip": ip, "mac": mac})
+# print(output)
+import subprocess
+try:
+    result = subprocess.run(['arp', '-a'] , capture_output= True, text=True)
+    lines = result.stdout.splitlines()
+except Exception as e:
+    print(f"Error executing arp command: {e}")
+    lines = []
 output = []
 for line in lines:
     if 'dynamic' in line:
         ip, mac = line.split()[:2]
         output.append({"ip": ip, "mac": mac})
-print(output)
+print (output)
