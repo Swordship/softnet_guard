b639b2b63819d1bb229c93353759fc7d84663208
Monish <monish635112@gmail.com>
2026-03-10 18:38:14 +0530

    traffic_stats table stores information about network traffic statistics for devices connected to the network,including timestamps, device IP addresses, packets sent and received,bytes sent and received, destination IP addresses, and ports used.

---
+++ database/traffic_Stats.sql
@@ -0,0 +1,16 @@
+-- SQL script to create the 'traffic_stats' table in the database 
+-- if it does not already exist. This table will store
+-- information about network traffic statistics for devices connected to the network,
+-- including timestamps, device IP addresses, packets sent and received,
+-- bytes sent and received, destination IP addresses, and ports used.
+CREATE table if not exists traffic_stats (
+ID integer PRIMARY KEY AUTOINCREMENT,
+timestamp text not null,
+device_ip text not null,
+packets_sent integer not null ,
+packets_recived  integer not null,
+bytes_sent integer not null,
+bytes_recived integer not null,
+destination_ip text null,
+port integer null
+);
