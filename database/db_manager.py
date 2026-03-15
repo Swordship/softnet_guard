281df3f3eb9550c6ba41d2bf79424e7cc24f44be
Monish <monish635112@gmail.com>
2026-03-15 20:07:28 +0530

    there are different tables in this folder , instead of hardcode the table in here in one , i just runn a look in this folder that contains all the .sql file to work with creating the table .

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 DB_PATH = "softnet_guard.db"
 
 def get_connection():
@@ -9,6 +9,25 @@ def get_connection():
     except sqlite3.Error as e:
         print(f"[DB ERROR] Could not connect: {e}")
         return None
+def initialize_db():
+    conn = get_connection()
+    if not conn:
+        return
+    
+    sql_folder = os.path.dirname(__file__)
+    
+    try:
+        for filename in os.listdir(sql_folder):
+            if filename.endswith(".sql"):
+                file_path = os.path.join(sql_folder, filename)
+                with open(file_path, 'r') as sql_file:
+                    sql_script = sql_file.read()
+                    conn.executescript(sql_script)
+                    print(f"Executed {filename}")
+    except Exception as e:
+        print(f"[ERROR] Failed to read or execute SQL files: {e}")
+    finally:
+        conn.close()
 if __name__ == "__main__":
     conn = get_connection()
     if conn:
