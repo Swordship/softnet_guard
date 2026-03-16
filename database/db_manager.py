e6e6ce92b19ca19689a4f87d59d38c7ec21b6c4c
Monish <monish635112@gmail.com>
2026-03-16 11:47:06 +0530

    feat : create insert device function to insert device data into device table

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,5 +1,7 @@
+import datetime
 import sqlite3
 import os
+from time import timezone
 DB_PATH = "softnet_guard.db"
 
 def get_connection():
@@ -26,6 +28,29 @@ def initialize_db():
                     print(f"Executed {filename}")
     except Exception as e:
         print(f"[ERROR] Failed to read or execute SQL files: {e}")
+    finally:
+        conn.close()
+def insert_device(ip_address, mac_address, host_name=None, vendor=None, device_type=None):
+    conn = get_connection()
+    if not conn:
+        return False
+    current_time = datetime.now(timezone.utc).isoformat()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO devices (ip_address, mac_address, host_name, vendor, device_type ,first_seen, last_seen)
+            VALUES (?, ?, ?, ?, ?, ?, ?) on conflict(mac_address) DO UPDATE SET 
+                    ip_address=excluded.ip_address, 
+                    host_name=excluded.host_name,
+                    vendor=excluded.vendor,
+                    device_type=excluded.device_type,
+                    last_seen = excluded.last_seen
+        """, (ip_address, mac_address, host_name, vendor, device_type,current_time,current_time))
+        conn.commit()
+        return True
+    except sqlite3.Error as e:
+        print(f"[DB ERROR] Failed to insert device: {e}")
+        return False
     finally:
         conn.close()
 if __name__ == "__main__":
