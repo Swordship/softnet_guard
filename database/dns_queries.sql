8092d6b54c258193c2117b9656ea79a0c9ea9cf1
Monish <monish635112@gmail.com>
2026-03-16 09:47:03 +0530

    add refactor query for dns_queries  table and commanded old table .

--- database/dns_queries.sql
+++ database/dns_queries.sql
@@ -4,11 +4,22 @@
 -- including timestamps, device IP addresses, queried domains,
 -- query types, and responses received. This information can be
 -- useful for monitoring and analyzing DNS traffic for potential security threats or anomalies.
-CREATE table if not exists dns_queries (
-id integer PRIMARY KEY AUTOINCREMENT,
-timestamp text not null,
-device_ip text not null,
-domain text not null,
-queries_type text null ,
-response text null
+-- CREATE table if not exists dns_queries (
+-- id integer PRIMARY KEY AUTOINCREMENT,
+-- timestamp text not null,
+-- device_ip text not null,
+-- domain text not null,
+-- queries_type text null ,
+-- response text null
+-- );
+CREATE TABLE IF NOT EXISTS dns_queries (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+queried_at TEXT NOT NULL,
+device_id INTEGER NOT NULL,
+domain TEXT NOT NULL,
+query_type TEXT,
+response TEXT,
+created_at TEXT NOT NULL DEFAULT (datetime('now')),
+FOREIGN KEY(device_id) REFERENCES devices(id) ON DELETE CASCADE
 );
+CREATE INDEX IF NOT EXISTS idx_dns_domain ON dns_queries(domain);
