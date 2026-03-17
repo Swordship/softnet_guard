1141993d122263fb02b872d14bf27182d6e7d6e4
Monish <monish635112@gmail.com>
2026-03-17 21:51:37 +0530

    test the psutil to scan the network because the scapy is good enough to , liket can't get the netmask so this one does that

--- test/traffic_analyser.py
+++ test/traffic_analyser.py
@@ -1,3 +1,5 @@
 import psutil
-print(psutil.net_io_counters())
-print(psutil.net_connections(kind='inet')[:3])
+# print(psutil.net_io_counters())
+# print(psutil.net_connections(kind='inet')[:3])
+interfaces = psutil.net_if_addrs()
+print(interfaces)
